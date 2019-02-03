0e26b5169e0224b10a6be13ee078353664894e19
Brandon Kimberly <bk685616@ohio.edu>
2019-02-03 17:23:03 -0500

    Create replace.h

---
+++ include/replace.h
@@ -0,0 +1,14 @@
+////////////////////////////////////////////////
+//  Name: Brandon Kimberly  Date: 01/27/2019  //
+//  This is the header file for homework 1    //
+//  which contains the declaration for the    //
+//  replace function.                         //
+////////////////////////////////////////////////
+#include<vector>
+#include<string>
+#ifndef REPLACE_H
+#define REPLACE_H
+
+std::vector<std::string> replace(std::vector<std::string>& text, std::string target, std::string replace);
+
+#endif
